d9a48e00cb7db0492a8d932ed60e0f3247bed276
k1rilo <kirill.ochkurov1@yandex.ru>
2024-10-14 23:06:35 +0300

    Adds SuperUser and models

--- shop_comp/comp/models.py
+++ shop_comp/comp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+    info = models.TextField(blank=True)
+    price = models.IntegerField()
+    
+    
+    def __str__(self) -> str:
+        return self.title
+    
+    
+class Category(models.Model):
+    name = models.CharField(max_length=50, db_index=True)
+    key = models.ForeignKey()
+    
--- shop_comp/comp/views.py
+++ shop_comp/comp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Product, Category
 
-# Create your views here.
+def build_template(lst: list, cols: int) -> list[list]:
+    new_list = [lst[i:i + cols] for i in range(0,len(lst), cols)]
+    return new_list
+
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 
+                  'comp/product_list.html', 
+                  context={'product_list': build_template(products, 3)})
+
+def category_list(request):
+    categories = Category.objects.all
+    return categories
