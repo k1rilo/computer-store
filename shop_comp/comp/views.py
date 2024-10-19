8ffa039a4386e6e5c9095fbf487dec3caf6ad358
k1rilo <kirill.ochkurov1@yandex.ru>
2024-10-19 23:20:21 +0300

    adds search in the shop

--- shop_comp/comp/views.py
+++ shop_comp/comp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+from django.db.models import Q
 
 def build_template(lst: list, cols: int) -> list[list]:
     new_list = [lst[i:i + cols] for i in range(0,len(lst), cols)]
@@ -8,11 +9,18 @@ def build_template(lst: list, cols: int) -> list[list]:
 
 def product_list(request):
     categories = Category.objects.all()
-    products = Product.objects.all()
+    search_query = request.GET.get('search', None)
+    if search_query:
+        products = Product.objects.filter(
+            Q(title__icontains=search_query)
+            |
+            Q(info__icontains=search_query)
+        )
     return render(request, 
                   'comp/product_list.html', 
                   context={'product_list': build_template(products, 3),
                     'categories': categories})
+
 
 def product_detail(request, pk):
     categories = Category.objects.all()
